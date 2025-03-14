e3ab9f41785f4542d2d7ea0a78a45982d9389e32
ketaboi619 <165798197+ketaboi619@users.noreply.github.com>
2025-03-14 10:18:22 -0500

    Update Course.java

--- Course.java
+++ Course.java
@@ -1,5 +1,7 @@
 package com.example.ClassRoomAPI.models;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 
 public class Course {
@@ -8,8 +10,10 @@ public class Course {
     private String Name;
 
     @ManyToOne
+    @JoinColumn(name = "fk_docente",referencedColumnName = "id_Teacher")
+    @JsonBackReference
     Teacher teacher;
-    
+
     public Course(){}
 
     public Course(Integer idCourse, String name) {
@@ -31,8 +35,5 @@ public class Course {
 
     public void setName(String name) {
         Name = name;
-    }
-}
-
     }
 }
